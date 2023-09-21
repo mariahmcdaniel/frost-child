16ff2cbfab7beccfd81cca01c36329b146291c89
Mariah <111791344+mariahmcdaniel@users.noreply.github.com>
2023-09-21 13:23:34 -0400

    functions.php enqueue edited

--- functions.php
+++ functions.php
@@ -1,9 +1,17 @@
 <?php
-add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
-function my_theme_enqueue_styles() {
-	wp_enqueue_style( 'child-style',
-		get_stylesheet_uri(),
-		array( 'parenthandle' ),
-		wp_get_theme()->get( 'Version' )
-	);
+
+
+add_action( 'wp_enqueue_scripts', 'frost_child_theme_enqueue_styles' );
+
+
+function frost_child_theme_enqueue_styles() {
+
+
+wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
+
+
 }
+
+
+?>
+
