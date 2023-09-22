c0c6466131877ceaf607da02d5011f4c08e9d864
Mariah <111791344+mariahmcdaniel@users.noreply.github.com>
2023-09-21 22:12:05 -0400

    editing enqueue function - css not firing

--- functions.php
+++ functions.php
@@ -1,9 +1,15 @@
 <?php
-add_action( 'wp_enqueue_scripts', 'frost_child_theme_enqueue_styles' );
-function frost_child_theme_enqueue_styles() {
-	wp_enqueue_style( 'child-style',
-		get_stylesheet_uri(),
-		array( 'frost' ),
-		wp_get_theme()->get( 'Version' ) // This only works if you have Version defined in the style header.
-	);
+add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
+function my_theme_enqueue_styles() {
+    $parent_style = 'frost-style'; 
+
+    wp_enqueue_style( $parent_style,
+        get_template_directory_uri() . '/style.css'
+    );
+
+    wp_enqueue_style( 'child-style',
+        get_stylesheet_uri(),
+        array( $parent_style ),
+        '1.0.0' 
+    );
 }
