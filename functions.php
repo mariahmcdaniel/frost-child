4686932af53b6869ad27909ef0fc8ad6fc70d3a2
Mariah <111791344+mariahmcdaniel@users.noreply.github.com>
2023-09-21 21:44:45 -0400

    edited enqueue styles funtion

--- functions.php
+++ functions.php
@@ -1,17 +1,9 @@
 <?php
-
-
 add_action( 'wp_enqueue_scripts', 'frost_child_theme_enqueue_styles' );
-
-
 function frost_child_theme_enqueue_styles() {
-
-
-wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
-
-
+	wp_enqueue_style( 'child-style',
+		get_stylesheet_uri(),
+		array( 'frost' ),
+		wp_get_theme()->get( 'Version' ) // This only works if you have Version defined in the style header.
+	);
 }
-
-
-?>
-
