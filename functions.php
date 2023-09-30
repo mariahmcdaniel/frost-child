fee84a144d4d9d85939c6330b893716d290e9689
Mariah <111791344+mariahmcdaniel@users.noreply.github.com>
2023-09-30 15:07:50 -0400

    changed  to 'frost' in functions.php

--- functions.php
+++ functions.php
@@ -1,7 +1,7 @@
 <?php
 add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
 function my_theme_enqueue_styles() {
-    $parent_style = 'frost-style'; 
+    $parent_style = 'frost'; 
 
     wp_enqueue_style( $parent_style,
         get_template_directory_uri() . '/style.css'
